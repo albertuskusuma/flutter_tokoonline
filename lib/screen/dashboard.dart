cd148a2d073944768ab358beec281aa7dfa8abf0
albertuskusuma <albertussindhu15@gmail.com>
2021-03-07 20:04:45 +0700

    create launcher page

---
+++ lib/screen/dashboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Dashboard'),
+      ),
+    );
+  }
+}
